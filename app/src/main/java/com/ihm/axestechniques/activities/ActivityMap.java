c82732cc54e8bd7ed34ce8986d63709ffa94b9b7
Lucas RAKOTOMALALA <lucas.rakotomalala@yahoo.fr>
2020-04-01 15:43:31 +0200

    OpenStreetMap + Intent avec la localisation

---
+++ app/src/main/java/com/ihm/axestechniques/activities/ActivityMap.java
@@ -0,0 +1,80 @@
+package com.ihm.axestechniques.activities;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
+import com.ihm.axestechniques.R;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
+public class ActivityMap extends AppCompatActivity implements IActivityMap {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Configuration.getInstance()
+                .load(getApplicationContext(),
+                        PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        setContentView(R.layout.activity_map);
+        mapView = findViewById(R.id.osmap);
+
+        Intent intent = getIntent();
+        double latitude = intent.getDoubleExtra(LATITUDE_PARAMETER, 0.0);
+        double longitude = intent.getDoubleExtra(LONGITUDE_PARAMETER, 0.0);
+
+        mapView.setTileSource(TileSourceFactory.MAPNIK); // Render
+        mapView.setBuiltInZoomControls(true); // Zoom available
+
+        GeoPoint startPoint = new GeoPoint(latitude, longitude);
+
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(18.0);
+        mapController.setCenter(startPoint);
+
+        ArrayList<OverlayItem> items = new ArrayList<>();
+
+        items.add(new OverlayItem("Localisation actuelle", "", startPoint));
+
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<>(getApplicationContext(), items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            @Override
+            public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onItemLongPress(int index, OverlayItem item) {
+                return false;
+            }
+        });
+        mOverlay.setFocusItemsOnTap(true);
+        mapView.getOverlays().add(mOverlay);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+}
---
+++ app/src/main/java/com/ihm/axestechniques/activities/IActivityMap.java
@@ -0,0 +1,6 @@
+package com.ihm.axestechniques.activities;
+
+public interface IActivityMap {
+    String LATITUDE_PARAMETER = "latitude";
+    String LONGITUDE_PARAMETER = "longitude";
+}
