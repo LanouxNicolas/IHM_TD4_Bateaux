95458acc7821021df8c645964121eb1d9168ba45
LanouxNicolas <nicolas.lanoux@etu.univ-cotedazur.fr>
2020-05-10 12:19:23 +0200

    fix display envoyer

--- app/src/main/java/com/ihm/seawatch/activities/MainActivity.java
+++ app/src/main/java/com/ihm/seawatch/activities/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
     @Override
---
+++ app/src/main/java/com/ihm/seawatch/fragments/Toolbar.java
@@ -0,0 +1,31 @@
+package com.ihm.seawatch.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.myapplication.R;
+
+public class Toolbar extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.toolbar, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.AppName).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Toolbar.this)
+                        .navigate(R.id.action_FifthFragment_to_FirstFragment);
+            }
+        });
+    }
+}
