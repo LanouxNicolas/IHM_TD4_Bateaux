2f5c92f3fcce493bc15bf0418e823fe807e45406
LanouxNicolas <nicolas.lanoux@etu.univ-cotedazur.fr>
2020-05-12 15:15:24 +0200

    google map branch

--- app/src/main/java/com/ihm/seawatch/activities/MainActivity.java
+++ app/src/main/java/com/ihm/seawatch/activities/MainActivity.java
@@ -15,7 +15,7 @@ import androidx.appcompat.widget.Toolbar;
 import com.ihm.seawatch.R;
 
 public class MainActivity extends AppCompatActivity {
-
+//teste ici c'est la version avec google map
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
